32a6a8e2f936ae19fe4d45d16116df6069836af3
supelpiotr <48559432+supelpiotr@users.noreply.github.com>
2020-10-17 13:19:01 +0200

    Initial commit

---
+++ src/main/java/com/supelpiotr/restbankingapi/RestBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.supelpiotr.restbankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBankingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supelpiotr/restbankingapi/RestBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supelpiotr.restbankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
