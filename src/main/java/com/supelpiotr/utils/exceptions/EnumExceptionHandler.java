95c52f4e8ab1c51029a7cc1723ba4350059c228d
supelpiotr <48559432+supelpiotr@users.noreply.github.com>
2020-10-21 19:36:51 +0200

    adds access denied exception handler

--- src/main/java/com/supelpiotr/utils/exceptions/EnumExceptionHandler.java
+++ src/main/java/com/supelpiotr/utils/exceptions/EnumExceptionHandler.java
@@ -2,11 +2,14 @@ package com.supelpiotr.utils.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class EnumExceptionHandler {
@@ -23,6 +26,11 @@ public class EnumExceptionHandler {
     @ResponseBody
     public ErrorDTO currencyValidationError(HttpMessageNotReadableException ex) {
         return new ErrorDTO(ErrorConstants.ERR_MESSAGE_NOT_READABLE, ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ErrorDTO handleAccessDeniedException(Exception ex, HttpServletRequest request){
+        return new ErrorDTO(ErrorConstants.ERR_ACCESS_DENIED, ex.getMessage());
     }
 
 }
--- src/main/java/com/supelpiotr/utils/exceptions/ErrorConstants.java
+++ src/main/java/com/supelpiotr/utils/exceptions/ErrorConstants.java
@@ -4,6 +4,7 @@ public final class ErrorConstants {
 
     public static final String ERR_CONVERSION_NOT_SUPPORTED = "error.conversionNotSupported";
     public static final String ERR_MESSAGE_NOT_READABLE = "error.messageNotReadable";
+    public static final String ERR_ACCESS_DENIED = "error.accessDenied";
 
     private ErrorConstants() {
     }
